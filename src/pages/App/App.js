6c041c8a233ed7e7912fbafbac2f50f227fec899
Kamali Means <darenkmeans@gmail.com>
2020-10-11 18:00:59 -0700

    add route to signup

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Plants from '../Plants/Plants';
 import AddPlant from '../AddPlant/AddPlant';
 import Navigation from '../../components/Navigation/Navigation';
+import SignUpPage from '../SignUpPage/SignUpPage';
 
 function App() {
   return (
@@ -12,11 +13,14 @@ function App() {
       <header className="App-header">
       <Navigation />
       <Switch>
-        <Route exact path='/plants'>
+        <Route exact path="/plants">
           <Plants />
         </Route>
-        <Route exact path='/addplant'>
+        <Route exact path="/addplant">
           <AddPlant />
+        </Route>
+        <Route exact path="/signup">
+          <SignUpPage />
         </Route>
       </Switch>
       </header>
---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends Component {
+    render() {
+        return (
+            <>
+            SignUpPage Here!
+            </>
+        );
+    }
+}
+
+export default SignUpPage;
