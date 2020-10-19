c15b45ea57ccee9875eb4c1298d416689786eddd
Kamali Means <darenkmeans@gmail.com>
2020-10-18 21:14:26 -0700

    update styling

--- src/components/MessageBody/MessageBody.jsx
+++ src/components/MessageBody/MessageBody.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './MessageBody.css'
 
 function MessageBody({message}) {
     return (
-        <div className="card-subtitle">
+        <div className="card card-body message">
+            <div className='card-title'>
+            <h4>{message.sender.name} says: </h4>
+            </div>
+            <div className="card-text  message-body">
             {message.body}
+            </div>
         </div>
     )
 }
